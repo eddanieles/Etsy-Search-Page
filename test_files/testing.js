6f489ae660171619a2e0fdd783e1300e41366441
eddanieles <ed.danieles@yahoo.com>
2016-08-10 10:17:09 -0400

    added usernames

--- test_files/testing.js
+++ test_files/testing.js
@@ -17,13 +17,28 @@
                 dataType: 'jsonp',
 
                 success: function(data) {
-                  console.log(data);
-                  var itemData = data.results.map((item) => `<div class="item">
-                  <a href="${item.url}"><img src="${item.Images[0].url_170x135}" alt="" /></a>
-                  <p><span>${item.title}</span></p>
-                  <p>User: ${item.user_id}<span class="price">${parseFloat(item.price).toLocaleString('us-EN', { style: 'currency', currency: 'USD' })}</span></p>
-                  <div>`);
-                  $("#etsy-images").html(itemData);
+                  //console.log(data);
+                  var users = data.results.map((user) => user.user_id);
+                  //console.log(users);
+                  $.ajax({
+                    url: `https://openapi.etsy.com/v2/users/${users}.js?api_key=kblh8c5ktm9s8x1qv5o95yyz`,
+                    dataType: 'jsonp',
+
+                    success: function(brian){
+                      console.log(brian);
+                      var userData = brian.results.map((user) => user.login_name);
+                      console.log(userData);
+
+                      var itemData = data.results.map((item, index) => `<div class="item">
+                      <a href="${item.url}"><img src="${item.Images[0].url_170x135}" alt="" /></a>
+                      <p><span>${item.title}</span></p>
+                      <p>User: ${userData[index]}<span class="price">${parseFloat(item.price).toLocaleString('us-EN', { style: 'currency', currency: 'USD' })}</span></p>
+                      <div>`);
+                      $("#etsy-images").html(itemData);
+                    }
+                  })
+
+
 
                 }
             });
@@ -31,7 +46,7 @@
 
             return false;
         })
-
+/*
         api_key = "kblh8c5ktm9s8x1qv5o95yyz";
         estyUserKeywords = $("userKeywords").val();
         estyUserUrl = "https://openapi.etsy.com/v2/users?keywords=" + estyUserKeywords + "&api_key=" + api_key;
@@ -46,7 +61,7 @@
 
             }
         });
-
+*/
 
     }); //end of document.ready
 
